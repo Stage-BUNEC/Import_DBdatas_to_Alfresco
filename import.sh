fbae0e775c6302a17750ad44cec6b3d34d7bb59a
Nanfack Steve <nanfacksteve7@gmail.com>
2022-09-29 17:23:20 +0100

    send Datas - Pending

--- import.sh
+++ import.sh
@@ -1,84 +1,90 @@
 #!/bin/bash
 
-# [ LastUpdate ]  : 27-09-2022
-# [ Description ] :
+# [ LastUpdate ]  : 29-09-2022
+# [ Description ] : Script d'envoie des donnees dans Alfresco
 # [ Author(s) ]   : Mr MANI / NANFACK STEVE
 
 #csvfile=en4201_reprise_naissance.csv
 csvFile=$1
-#source fonctions.sh
 
 # Recupere Nbre de Ligne
 #nbrLign=$(grep -c "" "$csvFile")
-#echo "$nbrLign"
 
 # Recupere Nbre de Champ
 # nbrColn=$(awk -F# '{print NF; exit;}' "$csvFile")
-# echo "$nbrColn"
 
-affichInfos() {
-    echo -e "\nnom: $nom \nprenom: $prenom \nDateNaiss: $dateNaiss \nLieuNaiss: $lieuNaiss \nSexe: $sexe 
-    \nNomsPere: $nomsPere \nDateNaiss: $dateNaissPere \nNe Vers: $neVersPere \nLieuNaiss: $lieuNaissPere \nDomicilie: $domicilePere \nProf: $professionPere \nNationalite: $nationalitePere \ndocRef: $docRefPere
-    \nNomsMere: $nomsMere \nDateNaiss: $dateNaissMere \nNe Vers: $neVersMere \nLieuNaiss: $lieuNaissMere \nDomicilie: $domicileMere \nProf: $professionMere \nNationalite: $nationaliteMere \ndocRef: $docRefMere
-    \nNomDeclarant: $nomDeclarant \nQual.Declar.: $qualDeclarant \nData Sign: $dateSignature \nDresse Le: $dresseLe \nOfficier: $officier \nSecretaire: $secretaire \nMention Marg: $mentionMarg
-    \nNumero Acte: $num_acte \nRegistre: $registre \nNom PDF: $fileName \nChemin: $path"
-}
+# creation Tablau Associatif
+declare -A postFiedls=()
+
 i=1
 while read -r ligne; do
 
-    IFS="#" read -r -a datas <<<"$(echo "$ligne")"
+    IFS="#" read -r -a datas <<<"$(echo "$ligne" | cat)"
 
     #-------- Extraction des Meta donnees -----------#
 
     # infos enfants
-    nom="${datas[0]}"
-    prenom="${datas[1]}"
-    dateNaiss="${datas[2]}"
-    lieuNaiss="${datas[3]}"
-    sexe="${datas[4]}"
+    postFiedls['nom']="${datas[0]}"
+    postFiedls['prenom']="${datas[1]}"
+    postFiedls['dateNaiss']="${datas[2]}"
+    postFiedls['lieuNaiss']="${datas[3]}"
+    postFiedls['sexe']="${datas[4]}"
 
     #infos Pere enfant
-    nomsPere="${datas[5]}"
-    dateNaissPere="${datas[6]}"
-    neVersPere="${datas[7]}"
-    lieuNaissPere="${datas[25]}"
-    domicilePere="${datas[8]}"
-    professionPere="${datas[10]}"
-    nationalitePere="${datas[9]}"
-    docRefPere="${datas[24]}"
+    postFiedls['nomsPere']="${datas[5]}"
+    postFiedls['dateNaissPere']="${datas[6]}"
+    postFiedls['neVersPere']="${datas[7]}"
+    postFiedls['lieuNaissPere']="${datas[25]}"
+    postFiedls['domicilePere']="${datas[8]}"
+    postFiedls['professionPere']="${datas[10]}"
+    postFiedls['nationalitePere']="${datas[9]}"
+    postFiedls['docRefPere']="${datas[24]}"
 
     #infos Mere enfant
-    nomsMere="${datas[11]}"
-    dateNaissMere="${datas[12]}"
-    neVersMere="${datas[13]}"
-    lieuNaissMere="${datas[14]}"
-    domicileMere="${datas[15]}"
-    professionMere="${datas[16]}"
-    nationaliteMere="${datas[17]}"
-    docRefMere="${datas[18]}"
+    postFiedls['nomsMere']="${datas[11]}"
+    postFiedls['dateNaissMere']="${datas[12]}"
+    postFiedls['neVersMere']="${datas[13]}"
+    postFiedls['lieuNaissMere']="${datas[14]}"
+    postFiedls['domicileMere']="${datas[15]}"
+    postFiedls['professionMere']="${datas[16]}"
+    postFiedls['nationaliteMere']="${datas[17]}"
+    postFiedls['docRefMere']="${datas[18]}"
 
     #infos Agents
-    nomDeclarant="${datas[20]}"
-    qualDeclarant="${datas[21]}"
-    dateSignature="${datas[22]}"
-    dresseLe="${datas[26]}"
-    officier="${datas[27]}"
-    secretaire="${datas[28]}"
-    mentionMarg="${datas[29]}"
+    postFiedls['nomDeclarant']="${datas[20]}"
+    postFiedls['qualDeclarant']="${datas[21]}"
+    postFiedls['dateSignature']="${datas[22]}"
+    postFiedls['dresseLe']="${datas[26]}"
+    postFiedls['officier']="${datas[27]}"
+    postFiedls['secretaire']="${datas[28]}"
+    postFiedls['mentionMarg']="${datas[29]}"
 
     #infos fichier
-    num_acte="${datas[23]}"
-    registre="${datas[30]}"
-    fileName="${datas[31]}"
-    path="${datas[32]}"
+    postFiedls['num_acte']="${datas[23]}"
+    postFiedls['registre']="${datas[30]}"
+    postFiedls['fileName']="${datas[31]}"
+    postFiedls['path']="${datas[32]}"
 
-    #affichInfos
-    #if [ $i -eq 1 ]; then break; fi;i=$((i + 1))
+    #for key in ${!postFiedls[*]}; do echo "$key --> ${postFiedls[$key]}"; done
 
     #------- Connexion a Alfrsco --------------#
 
-    curl -X POST http://172.16.1.99:8080/alfresco/api/-default-/public/authentication/versions/1/tickets -H "Content-Type: application/json" -d '{"userId": "admin", "password": "bunec"}'
+    # recupere ticket
+    reponse=$(curl -s -X POST http://172.16.1.99:8080/alfresco/api/-default-/public/authentication/versions/1/tickets -H "Content-Type: application/json" -d '{"userId": "admin", "password": "bunec"}')
+    ticket=$(echo "$reponse" | cut -d'"' -f6)
+
+    # recupere l'ID du dossier Partage/Shared
+    rep=$(curl -s -X GET "http://172.16.1.99:8080/alfresco/api/-default-/public/alfresco/versions/1/nodes/-root-/children?alf_ticket=${ticket}")
+    sharedID=$(echo "$rep" | cut -d'"' -f258)
+
+    # creation noeud/Dossier dans Partage/Shared
+    dossier=${postFiedls['registre']}
+    rep2=$(curl -s -X POST -H "Content-Type: application/json" "http://172.16.1.99:8080/alfresco/api/-default-/public/alfresco/versions/1/nodes/${sharedID}/children?alf_ticket=${ticket}" -d '{"name":"'"$dossier"'", "nodeType":"cm:folder"}')
+
+    # Envoie des donnees
+    ## CODE A ECRIRE
+
+    if [ $i -eq 1 ]; then break; fi
+    i=$((i + 1))
 
 done < <(grep -v "nomsenfant#prenomsenfant#datenaissenfant#lieunaissenfant#sexe#" "$csvFile")
-
-#echo "-" "${#datas[@]}"
