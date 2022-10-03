c710da56b36cff09f5d11caabd13decf5d3684a7
Nanfack Steve <nanfacksteve7@gmail.com>
2022-10-03 11:03:23 +0100

    send Datas - OK

--- import.sh
+++ import.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# [ LastUpdate ]  : 29-09-2022
-# [ Description ] : Script d'envoie des donnees dans Alfresco
+# [ LastUpdate ]  : 03-10-2022
+# [ Description ] : Script d'envoie des donnees dans Alfresco (PDF + metadonnees)
 # [ Author(s) ]   : Mr MANI / NANFACK STEVE
 
 #csvfile=en4201_reprise_naissance.csv
@@ -13,7 +13,7 @@ csvFile=$1
 # Recupere Nbre de Champ
 # nbrColn=$(awk -F# '{print NF; exit;}' "$csvFile")
 
-# creation Tablau Associatif
+# Creation Tablau Associatif
 declare -A postFiedls=()
 
 i=1
@@ -23,14 +23,14 @@ while read -r ligne; do
 
     #-------- Extraction des Meta donnees -----------#
 
-    # infos enfants
+    # Infos enfants
     postFiedls['nom']="${datas[0]}"
     postFiedls['prenom']="${datas[1]}"
     postFiedls['dateNaiss']="${datas[2]}"
     postFiedls['lieuNaiss']="${datas[3]}"
     postFiedls['sexe']="${datas[4]}"
 
-    #infos Pere enfant
+    # Infos Pere enfant
     postFiedls['nomsPere']="${datas[5]}"
     postFiedls['dateNaissPere']="${datas[6]}"
     postFiedls['neVersPere']="${datas[7]}"
@@ -40,7 +40,7 @@ while read -r ligne; do
     postFiedls['nationalitePere']="${datas[9]}"
     postFiedls['docRefPere']="${datas[24]}"
 
-    #infos Mere enfant
+    # Infos Mere enfant
     postFiedls['nomsMere']="${datas[11]}"
     postFiedls['dateNaissMere']="${datas[12]}"
     postFiedls['neVersMere']="${datas[13]}"
@@ -50,7 +50,7 @@ while read -r ligne; do
     postFiedls['nationaliteMere']="${datas[17]}"
     postFiedls['docRefMere']="${datas[18]}"
 
-    #infos Agents
+    # Infos Agents
     postFiedls['nomDeclarant']="${datas[20]}"
     postFiedls['qualDeclarant']="${datas[21]}"
     postFiedls['dateSignature']="${datas[22]}"
@@ -59,32 +59,42 @@ while read -r ligne; do
     postFiedls['secretaire']="${datas[28]}"
     postFiedls['mentionMarg']="${datas[29]}"
 
-    #infos fichier
+    # Infos fichier
     postFiedls['num_acte']="${datas[23]}"
     postFiedls['registre']="${datas[30]}"
     postFiedls['fileName']="${datas[31]}"
     postFiedls['path']="${datas[32]}"
 
+    # Affichage Meta Donnees
     #for key in ${!postFiedls[*]}; do echo "$key --> ${postFiedls[$key]}"; done
 
     #------- Connexion a Alfrsco --------------#
 
-    # recupere ticket
+    # Recupere ticket
     reponse=$(curl -s -X POST http://172.16.1.99:8080/alfresco/api/-default-/public/authentication/versions/1/tickets -H "Content-Type: application/json" -d '{"userId": "admin", "password": "bunec"}')
     ticket=$(echo "$reponse" | cut -d'"' -f6)
 
-    # recupere l'ID du dossier Partage/Shared
+    # Recupere l'ID du dossier Partage/Shared
     rep=$(curl -s -X GET "http://172.16.1.99:8080/alfresco/api/-default-/public/alfresco/versions/1/nodes/-root-/children?alf_ticket=${ticket}")
     sharedID=$(echo "$rep" | cut -d'"' -f258)
 
-    # creation noeud/Dossier dans Partage/Shared
+    # Creation noeud/Dossier dans Partage/Shared
     dossier=${postFiedls['registre']}
     rep2=$(curl -s -X POST -H "Content-Type: application/json" "http://172.16.1.99:8080/alfresco/api/-default-/public/alfresco/versions/1/nodes/${sharedID}/children?alf_ticket=${ticket}" -d '{"name":"'"$dossier"'", "nodeType":"cm:folder"}')
+    echo "$rep2" | grep "statusCode"
 
     # Envoie des donnees
-    ## CODE A ECRIRE
+    curl -s -X POST -H "Content-Type: multipart/form-data" -F "filedata"="@${postFiedls['fileName']}" -F "relativePath"="${postFiedls['registre']}" \
+        -F "bc:numact"="${postFiedls['num_acte']}" -F "bc:firstname"="${postFiedls['nom']}" -F "bc:lastname"="${postFiedls['prenom']}" -F "bc:bornOnThe"="${postFiedls['dateNaiss']}" -F "bc:bornAt"="${postFiedls['lieuNaiss']}" -F "bc:sex"="${postFiedls['sexe']}" \
+        -F "bc:of"="${postFiedls['nomsPere']}" -F "bc:fOnThe"="" -F "bc:fAt"="${postFiedls['lieuNaissPere']}" -F "bc:fresid"="${postFiedls['domicilePere']}" -F "bc:foccupation"="${postFiedls['professionPere']}" -F "bc:fnationality"="${postFiedls['nationalitePere']}" -F "bc:fdocref"="${postFiedls['docRefPere']}" \
+        -F "bc:mof"="${postFiedls['nomsMere']}" -F "bc:mAt"="${postFiedls['lieuNaissMere']}" -F "bc:mOnThe"="" -F "bc:mresid"="${postFiedls['domicileMere']}" -F "bc:mOccupation"="${postFiedls['professionMere']}" -F "bc:mnationality"="${postFiedls['nationaliteMere']}" -F "bc:mdocref"="${postFiedls['docRefMere']}" \
+        -F "bc:drawingUp"="${postFiedls['dresseLe']}" -F "bc:ondecof"="${postFiedls['qualDeclarant']}" -F "bc:byUs"="${postFiedls['officier']}" -F "bc:assistedof"="${postFiedls['secretaire']}" -F "bc:onthe"="${postFiedls['dateSignature']}" -F "bc:mentionMarg"="${postFiedls['mentionMarg']}" \
+        "http://172.16.1.99:8080/alfresco/api/-default-/public/alfresco/versions/1/nodes/-shared-/children?alf_ticket=${ticket}"
 
     if [ $i -eq 1 ]; then break; fi
     i=$((i + 1))
 
 done < <(grep -v "nomsenfant#prenomsenfant#datenaissenfant#lieunaissenfant#sexe#" "$csvFile")
+
+# Suppression tableau
+unset postFiedls
